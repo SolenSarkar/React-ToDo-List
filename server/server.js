9e7e0b2b97b7700ccfb6f7b42df562c09650932f
SolenSarkar <solensarkar3938@gmail.com>
2026-08-06 22:20:47 +0530

    new

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import path from 'path';
+import path from 'path';   
 import { fileURLToPath } from 'url';
 import todoRoutes from './routes/todos.js';
 
